c497704e6c64bdc2d08a75bb4a9392b49185dc27
leixw <leixw@autoai.com>
2025-10-31 18:35:44 +0800

    Vue核心虚拟Dom和 diff 算法

---
+++ vue-demo/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <div v-for="item in arr">{{ item }}</div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const arr:string[] = ['A','B','C','D']
+arr.splice(2,0,'DDD')
+</script>
+
+<style></style>
